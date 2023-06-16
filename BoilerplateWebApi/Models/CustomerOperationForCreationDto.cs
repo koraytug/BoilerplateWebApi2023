44789d9eac9d8df90c15fe5d5cadd93055f9a983
koraytug <koraytug@hotmail.com>
2023-06-16 14:02:27 +0300

    KT - Added validation

--- BoilerplateWebApi/Models/CustomerOperationForCreationDto.cs
+++ BoilerplateWebApi/Models/CustomerOperationForCreationDto.cs
@@ -1,7 +1,11 @@
-﻿namespace BoilerplateWebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoilerplateWebApi.Models
 {
     public class CustomerOperationForCreationDto
     {
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
         public double? Price { get; set; } = 0;
     }
